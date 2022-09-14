89273fbca2b5c47435513dfc6291d3f8d43e6a44
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2022-09-15 02:25:31 +0300

    add without scrollBy

--- src/index.js
+++ src/index.js
@@ -19,12 +19,12 @@ let page = "";
 let fromInput = "";
 
 function onScroll() {
-    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+//     const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
 
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-});
+//     window.scrollBy({
+//         top: cardHeight * 2,
+//         behavior: "smooth",
+// });
     const documentRect = document.documentElement.getBoundingClientRect();
 
     if (documentRect.bottom < document.documentElement.clientHeight + 10) {
